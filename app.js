8897b68604f30b5846fcf7355ac1b8df53c11e93
Ankur reyes <Ankur reyes>
2016-10-26 22:46:57 +0530

    last commit

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var app = express();
 var multer  = require('multer');
 var upload = multer({ dest: 'uploads/' });
+var t;
 
 app.use(express.static(path.join(__dirname, 'views')));
 
